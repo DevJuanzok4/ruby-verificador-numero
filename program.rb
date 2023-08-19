66eb1b5ae4007001c65b802bab5a055b6ff2da1a
Juan C <121655192+DevJuanzok4@users.noreply.github.com>
2023-08-19 00:37:37 +0000

    chore: adição da estrutura

--- program.rb
+++ program.rb
@@ -1,3 +1,12 @@
 
 puts "Digite um número:"
 nr = gets.chomp.to_i
+
+if nr > 0
+    puts "O número é positivo."
+elsif
+    nr < 0
+    puts "o Número é negativo"
+else
+    puts "O número é zero"
+end
